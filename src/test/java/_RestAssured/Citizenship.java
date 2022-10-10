6e9b073ea3acc9b07552fed58da32c019fbb5d4a
ahmet <ahmetdem6325@gmail.com>
2022-10-10 18:47:03 +0300

    restAssured 2

--- src/test/java/_RestAssured/Citizenship.java
+++ src/test/java/_RestAssured/Citizenship.java
@@ -11,6 +11,14 @@ public class Citizenship {
         setShortName(shortName);
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
--- src/test/java/_RestAssured/CitizenshipTest.java
+++ src/test/java/_RestAssured/CitizenshipTest.java
@@ -17,8 +17,7 @@ public class CitizenshipTest {
 
     Cookies cookies;
 
-   @BeforeClass
-
+    @BeforeClass
     public void login() {
         baseURI = "https://demo.mersys.io";
         Map<String, String> credentials = new HashMap<>();
@@ -26,7 +25,7 @@ public class CitizenshipTest {
         credentials.put("password", "Richfield2020!");
         credentials.put("rememberMe", "true");
 
-       cookies= given()
+        cookies = given()
                 .body(credentials)
                 .contentType(ContentType.JSON)
                 .when()
@@ -34,41 +33,120 @@ public class CitizenshipTest {
                 .then()
                 .statusCode(200)
                 //.log().body()
-               .extract().response().getDetailedCookies()
+                .extract().response().getDetailedCookies()
         ;
-       System.out.println("cookies = " + cookies);
+        //System.out.println("cookies = " + cookies);
     }
 
-    public String getRandom(int value){
-       return RandomStringUtils.randomAlphabetic(value);
+    public String getRandom(int value) {
+        return RandomStringUtils.randomAlphabetic(value);
 
     }
 
-    String name=getRandom(8);
-   String shortName=getRandom(4);
-   String citizenShipId;
+    String name = getRandom(8);
+    String shortName = getRandom(4);
+    String citizenShipId;
 
     @Test
-    public void createCitizenship(){
-       Citizenship citizenship=new Citizenship(name,shortName);
+    public void createCitizenship() {
+        Citizenship citizenship = new Citizenship(name, shortName);
 
-       citizenShipId= given()
-               .cookies(cookies)
-               .body(citizenship)
-               .contentType(ContentType.JSON)
+        citizenShipId = given()
+                .cookies(cookies)
+                .body(citizenship)
+                .contentType(ContentType.JSON)
 
                 .when()
-               .post("/school-service/api/citizenships")
+                .post("/school-service/api/citizenships")
 
                 .then()
-               .statusCode(201)
-               .log().body()
-               .body("name",equalTo(name))
-               .extract().jsonPath().getString("id")
+                .statusCode(201)
+                .log().body()
+                .body("name", equalTo(name))
+                .extract().jsonPath().getString("id")
         ;
         System.out.println("name manual = " + name);
+    }
+
+    @Test(dependsOnMethods = "createCitizenship")
+    public void createCitizenshipNegative() {
+        Citizenship citizenship = new Citizenship(name, shortName);
+
+        given()
+                .cookies(cookies)
+                .body(citizenship)
+                .contentType(ContentType.JSON)
+
+                .when()
+                .post("/school-service/api/citizenships")
+
+                .then()
+                .statusCode(400)
+                .body("message", equalTo("The Citizenship with Name \"" + name + "\" already exists."))
+
+        ;
+        System.out.println("name manual = " + name);
+    }
+
+    @Test(dependsOnMethods = "createCitizenship")
+    public void updateCitizenship() {
+
+        String updatedName = getRandom(8);
+        Citizenship citizenship = new Citizenship(updatedName, shortName);
+        citizenship.setId(citizenShipId);
+
+        given()
+                .cookies(cookies)
+                .body(citizenship)
+                .contentType(ContentType.JSON)
+                .when()
+                .put("/school-service/api/citizenships")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo(updatedName))
+        ;
+        System.out.println("updatedName = " + updatedName);
+    }
+
+    @Test(dependsOnMethods = "updateCitizenship")
+    public void deleteById(){
+
+        given()
+                .cookies(cookies)
+                .pathParam("citizenShipId",citizenShipId)
+
+                .when()
+                .delete("/school-service/api/citizenships/{citizenShipId}")
+
+                .then()
+                .statusCode(200)
+                .log().body()
+                ;
+    }
+    @Test(dependsOnMethods = "deleteById")
+    public void deleteByIdNegative(){
+
+        given()
+                .cookies(cookies)
+                .pathParam("citizenShipId",citizenShipId)
+
+                .when()
+                .delete("/school-service/api/citizenships/{citizenShipId}")
+
+                .then()
+                .statusCode(400)
+                .log().body()
+        ;
+
 
     }
+
 
 
 }
+
+
+
+
+
+
